b155b122e7eee769ef9ee34f4a73affb363cc38f
MansourAlawi <mansour.alawi@gmail.com>
2022-05-13 12:42:30 +0800

    change timeout

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  timeout: 60000,
+  timeout: 600000,
   use: {
     // Browser options
     headless: true,
